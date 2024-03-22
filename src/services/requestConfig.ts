210d9ab5f2d20d86a8a9a426749031ca26a2a73e
harleykwen <harleykwen.work@gmail.com>
2024-03-23 01:57:27 +0700

    axios

---
+++ src/services/requestConfig.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+
+const requestConfig = axios.create()
+
+// Add a request interceptor
+requestConfig.interceptors.request.use(function (config: InternalAxiosRequestConfig<any>) {
+    // Do something before request is sent
+    if (config.withCredentials) {
+        config.headers.Authorization = '' // Put token here
+    }
+    return config
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error)
+})
+
+// Add a response interceptor
+requestConfig.interceptors.response.use(function (response: AxiosResponse<any, any>) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response
+}, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error)
+})
+
+export default requestConfig
